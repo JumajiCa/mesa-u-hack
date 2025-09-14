e0be4fce911d554611a8009d7638148acb60cf3f
CoraRui <cameronxensel@proton.me>
2025-09-13 21:55:16 -0700

    commit3

--- electric-simulations/assets/particle/particle.gd
+++ electric-simulations/assets/particle/particle.gd
@@ -1,11 +1,12 @@
 extends Node2D
 class_name particle
 
-
-
 @export var vel : Vector2 = Vector2(0,0)
+var acc : Vector2 = Vector2(0,0)
+var test_vel : float = 0
 @export var charge : float = 1.0 * (1/pow(10, 4))
-@export var mass : float = 1.0 * pow(10,5)
+@export var mass : float = 1.0
+@export var efield : float = 1.0 * pow(10, 4)
 
 @export var moveActive : bool = false
 
@@ -18,21 +19,29 @@ const K : int = 8.99 * pow(10, 9)
 #and also the vel should be modulated by the acceleration, which will be
 #calculated by its position related to the other particle
 
+var timer : float = 0
+
 func _process(delta : float) -> void:
 	if !moveActive:
 		return
-	accelerate()
-	move()
+	timer += delta
+	accelerate(delta)
+	move(delta)
 	
-func accelerate() -> void:
-	var acc : Vector2 = Vector2(0,0)
-	acc.x = clampf(charge*otherCharge.charge*K/mass*(global_position.x - otherCharge.global_position.x), -999, 999)
-	vel += Vector2(clampf(acc.x, -999999, 999999), clampf(acc.y, -999999, 999999))
-	print(acc)
+func accelerate(d : float) -> void:
+	vel.x = timer * (charge*efield)/mass
+	acc.x = clampf((charge*efield)/mass, -99999999, 999999999)
+	print("acceleration+ delta", acc.x, ":", d)
+
+	vel += Vector2(clampf(acc.x*d, -999999, 999999), clampf(acc.y, -999999, 999999))
+	#print("test_vel", test_vel)
+	#print("velocity: ", vel.x)
+	
 	# F = kqq/(r^2)
-	
-func move() -> void:
-	global_position += vel
+func move(delta : float) -> void:
+	global_position += vel * delta * 100
+	#100 is meters to pixels
 
 func reset() -> void:
 	vel = Vector2(0,0)
+	timer = 0
--- electric-simulations/charged-speed-sim/charged_speed_sim.gd
+++ electric-simulations/charged-speed-sim/charged_speed_sim.gd
@@ -10,7 +10,20 @@ class_name chargedspeedsim
 @onready var sim_timer : Timer = $sim_timer
 @onready var finish_label : Label = $finish_label
 
+@export var text_box : TextEdit
+
 @export var dThreshhold : float = 3		#number of pixels that the particle can be away.
+
+#extra info!
+@export var speed_label : Label 
+@export var time_label : Label
+@export var pos_label : Label
+
+var timer : float = 0
+var low_time : float = 0.95
+var high_time : float = 1.05
+
+var running : bool = false
 
 func _ready() -> void:
 	start()
@@ -20,21 +33,50 @@ func _input(event : InputEvent) -> void:
 		start()
 	if event.is_action_pressed("cancel"):
 		reset()
+		
+func _process(delta : float) -> void:
+	if running:
+		timer += delta 
+	update_labels()
 
 func start() -> void:
+	running = true
 	testCharge.moveActive = true
 	sim_timer.start()
+	testCharge.charge = float(text_box.text) * (1/pow(10, 4))
+	print("udpated charge: ", testCharge.charge)
+	print(float(testCharge.charge))
 
 func reset() -> void:
+	running = false
 	testCharge.moveActive = false
 	testCharge.reset()
 	testCharge.global_position = init_pos.global_position
+	timer = 0
+
+func stop() -> void:
+	testCharge.moveActive = false
+	finish_label.visible = true
+	print("simtimer out")
+	check_time()
+	running = false
+
+func update_labels() -> void:
+	speed_label.text = "Q2 Velocity: " + str(testCharge.vel.x)
+	time_label.text = "Time Elapsed: " + str(timer)
+	pos_label.text = "Distance from Start: " + str(testCharge.global_position.x - init_pos.global_position.x)
 
 func checkPosition() -> void:
 	if(testCharge.global_position.distance_to(target_point.global_position)) < dThreshhold:
 		finish_label.text = "You got it!"
 	else:
 		finish_label.text = "Hm, not quite right."
+
+func check_time() -> void: 
+	if low_time < timer && timer < high_time:
+		finish_label.text = "you got it!"
+	else:
+		finish_label.text = "not quite..."
 
 func _on_reset_button_button_down() -> void:
 	reset()
@@ -44,7 +86,7 @@ func _on_start_button_button_down() -> void:
 	finish_label.visible = false
 	
 func _on_sim_timer_timeout() -> void:
-	testCharge.moveActive = false
-	finish_label.visible = true
-	checkPosition()
-	print("simtimer out")
+	pass
+
+func _on_end_area_area_entered(area: Area2D) -> void:
+	stop()
